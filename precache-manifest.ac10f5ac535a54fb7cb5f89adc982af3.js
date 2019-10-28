f6d3564879cc36d3cfd3f32c19b7ab48402a149a
Indamix <dvetreti@gmail.com>
2019-10-28 22:30:52 +0100

    deploy

---
+++ precache-manifest.ac10f5ac535a54fb7cb5f89adc982af3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b702b24268112d98c5ff769c411fc66a",
+    "url": "/cocktails/index.html"
+  },
+  {
+    "revision": "49925694813ac8321a5f",
+    "url": "/cocktails/static/css/main.7a844315.chunk.css"
+  },
+  {
+    "revision": "85ff3361addc7bf61e46",
+    "url": "/cocktails/static/js/2.81da4f73.chunk.js"
+  },
+  {
+    "revision": "49925694813ac8321a5f",
+    "url": "/cocktails/static/js/main.b28394e6.chunk.js"
+  },
+  {
+    "revision": "f77b88c01f64bd932fb0",
+    "url": "/cocktails/static/js/runtime~main.50295a13.js"
+  },
+  {
+    "revision": "f1830fc0c6074e943e6338a599dbcd4f",
+    "url": "/cocktails/static/media/close.f1830fc0.svg"
+  },
+  {
+    "revision": "7df1ec766a53ca530058c562b4432326",
+    "url": "/cocktails/static/media/home.7df1ec76.svg"
+  }
+]);
